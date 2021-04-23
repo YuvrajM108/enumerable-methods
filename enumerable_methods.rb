a5b32ee1e9d35b642d270cf1c3e1c3f146e543ef
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-04-23 13:27:42 -0400

    Fix remaining linter errors

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -13,7 +13,7 @@ module Enumerable
 
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
-    
+
     arr = self
     i = 0
     while i < arr.length
@@ -22,5 +22,4 @@ module Enumerable
     end
     self
   end
-
 end
