c5e65e6ef1639597ef1d0fcf7c438dcc1cacbd90
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-04-23 13:22:08 -0400

    Add my_each_with_index method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,14 +1,24 @@
 module Enumerable
   def my_each
-    unless block_given?
-        return to_enum(:my_each)
-    end
+    return to_enum(:my_each) unless block_given?
     arr = self
     i = 0
     while i < arr.length
-        yield(arr[i])
-        i += 1
+      yield(arr[i])
+      i += 1
     end
     self
   end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+    arr = self
+    i = 0
+    while i < arr.length
+      yield(i)
+      i += 1
+    end
+    self
+  end
+
 end
