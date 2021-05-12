fcfa0f9d54e078b4eae5849a562fb0030734cdd3
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-05-12 12:30:04 -0400

    Create test case for #my_each

--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -1,4 +1,10 @@
 require_relative '../enumerable_methods'
 
 describe Enumerable do
+  describe "#my_each" do
+    it "prints every element in an array" do
+      arr = [1, 2, 3]
+      expect(arr.my_each {|x| puts x, "--" }).to eql(arr)
+    end
+  end
 end
