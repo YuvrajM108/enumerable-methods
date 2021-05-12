6381a77fe275cd5514546b39bae59fbac8ef3736
Daniel Samuel <dansam5000@gmail.com>
2021-05-12 18:26:55 +0100

    Add test for #my_each_with_index & #my_select

--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -7,4 +7,19 @@ describe Enumerable do
       expect(arr.my_each {|x| puts x, "--" }).to eql(arr)
     end
   end
+
+  describe "#my_each_with_index" do
+    let (:hash) { Hash.new }
+    it "prints every element with the index of the element" do
+      expect(%w(cat dog wombat).each_with_index { |item, index|
+      hash[item] = index}).to eql(["cat", "dog", "wombat"])
+    end
+  end
+
+  describe "#my_select" do
+    it "prints every element with the index of the element" do
+      arr =  [1,2,3,4,5]
+      expect(arr.select { |num|  num.even?  }).to eql([2, 4])
+    end
+  end
 end
